3ac38c5a9dc8fa285e9945352a156e7386b967a8
Iqbal Fauzi <iqbalfawz@gmail.com>
2017-09-04 15:56:16 +0700

    Add table index

--- modules/admin-rank-statistic/_db/install.sql
+++ modules/admin-rank-statistic/_db/install.sql
@@ -3,16 +3,10 @@ INSERT IGNORE INTO `user_perms` ( `name`, `group`, `role`, `about` ) VALUES
     ( 'update_site_rank', 'Site Rank', 'Management', 'Allow user to update site rank' );
 
 CREATE TABLE IF NOT EXISTS `site_rank` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `vendor` VARCHAR(50) NOT NULL,
     `international` INTEGER DEFAULT 0,
     `local` INTEGER DEFAULT 0,
-    `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    
-    PRIMARY KEY (
-        `id`,
-        `vendor`
-    )
-)
-    PARTITION BY KEY(`vendor`)
-    PARTITIONS 20;
+    `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE INDEX `by_vendor` ON `site_rank` ( `vendor` );
